a14927c19601b10fb3493e6c528daec77a0490ea
Haoling <noreply-haoling@fei-yen.jp>
2023-12-19 07:51:06 +0000

    commit

---
+++ vite.config.ts
@@ -0,0 +1,35 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import { fileURLToPath, URL } from 'url'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/main.tsx'),
+      name: "DanbooruReact",
+      fileName: "_",
+      formats: ["es"]
+    },
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name === 'style.css') return '_.css';
+          return assetInfo.name;
+        },
+      },
+    },
+    minify: false,
+    cssCodeSplit: false,
+  },
+  define: {
+    'process.env': {}
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
