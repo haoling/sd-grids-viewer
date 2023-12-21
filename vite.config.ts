75da5189be305602a65a5ab4b700ea0588cc1134
Haoling <noreply-haoling@fei-yen.jp>
2023-12-21 02:53:29 +0000

    use jsdeliver

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: 'https://cdn.jsdelivr.net/gh/haoling/sd-grids-viewer@main/dist/',
   build: {
     lib: {
       entry: resolve(__dirname, 'src/main.tsx'),
